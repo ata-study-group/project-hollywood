7444e0f2592f29120c49532cddcbdcfecec7c13c
Jasmine Chen <jasmiche@amazon.com>
2020-09-23 00:42:41 -0700

    Update starting page

--- src/App.js
+++ src/App.js
@@ -1,14 +1,14 @@
 import React from 'react';
-import logo from './logo.svg';
+import logo from './hollywood-star.png';
 import './App.css';
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
+        <img src={logo} className="App-logo" alt="hollywood-star" />
         <p>
-          Project Hollywood!
+          Project Hollywood
         </p>
       </header>
     </div>
