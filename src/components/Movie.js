633d2c77246696ae7b89031ed243ed80a8dd7ef7
jasmine1226 <jasmine1226@gmail.com>
2020-10-18 20:57:03 -0700

    Add search form

--- src/components/Movie.js
+++ src/components/Movie.js
@@ -1,13 +1,6 @@
 import React, { Component } from "react";
-import Button from "react-bootstrap/Button";
 
 class Movie extends Component {
-  showRating = (e) => {
-    var rating = document.getElementById("imdbRating");
-    rating.style.display = "block";
-    e.target.style.display = "none";
-  };
-
   render() {
     var movie = this.props.movie;
     return (
@@ -16,12 +9,7 @@ class Movie extends Component {
         <p>
           {movie.rated} | {movie.year} | {movie.country}
         </p>
-        <Button onClick={this.showRating} variant="light">
-          Click to reveal IMDB Rating
-        </Button>
-        <p id="imdbRating" style={{ display: "none" }}>
-          IMDB Rating: {movie.imdbRating}/10
-        </p>
+        <p id="imdbRating">IMDB Rating: {movie.imdbRating}/10</p>
       </div>
     );
   }
--- src/containers/GameContainer.js
+++ src/containers/GameContainer.js
@@ -1,8 +1,12 @@
 import React, { Component } from "react";
 import Movie from "../components/Movie.js";
+import Button from "react-bootstrap/Button";
+import Form from "react-bootstrap/Form";
+import Col from "react-bootstrap/Col";
 
 class GameContainer extends Component {
   state = {
+    showMovie: false,
     movie: {
       country: null,
       genre: null,
@@ -13,17 +17,19 @@ class GameContainer extends Component {
       year: null,
       imdbRating: 0.0,
     },
+    movieTitle: "",
   };
 
-  componentDidMount() {
+  loadMovie = (e) => {
+    e.preventDefault();
     fetch(
       "https://cors-anywhere.herokuapp.com/" + // CORS proxy to avoid browser error
         "http://hollywoodtest3-env.eba-gzbfvjh9.us-west-2.elasticbeanstalk.com/api/v1/movie"
     )
       .then((response) => response.json())
       .then((data) => {
-        console.log(data);
         this.setState({
+          showMovie: true,
           movie: {
             country: data.Country,
             genre: data.Genre,
@@ -35,13 +41,67 @@ class GameContainer extends Component {
             imdbRating: data.imdbRating,
           },
         });
+      })
+      .catch((error) => {
+        console.log(error);
       });
-  }
+  };
+
+  handleChange = (e) => {
+    this.setState({
+      movieTitle: e.target.value,
+    });
+  };
+
+  handleSubmit = (e) => {
+    e.preventDefault();
+    console.log("clicked!");
+    fetch(
+      "https://cors-anywhere.herokuapp.com/" +
+        "http://hollywoodtest3-env.eba-gzbfvjh9.us-west-2.elasticbeanstalk.com/api/v1/movie",
+      {
+        method: "POST",
+        headers: {
+          "Content-Type": "application/json",
+          "Cache-Control": "no-cache",
+          "User-Agent": "PostmanRuntime/7.26.7",
+          Accept: "*/*",
+        },
+        body: JSON.stringify({ movieTitle: this.state.movieTitle }),
+      }
+    )
+      .then((response) => console.log(response))
+      .catch((error) => {
+        console.log(error);
+      });
+  };
 
   render() {
     return (
       <div>
-        <Movie movie={this.state.movie} />
+        <Button onClick={this.loadMovie} variant="light">
+          Pick a random Movie!
+        </Button>
+        <br /> <br />
+        <Form onSubmit={this.handleSubmit}>
+          <Form.Row className="justify-content-center">
+            <Col xs="auto">
+              <Form.Control
+                className="mb-2"
+                id="inlineFormInput"
+                placeholder="Movie Title"
+                onChange={this.handleChange}
+              />
+            </Col>
+            <Col xs="auto">
+              <Button type="submit" className="mb-2" variant="light">
+                Search
+              </Button>
+            </Col>
+          </Form.Row>
+        </Form>
+        <br />
+        {this.state.showMovie ? <Movie movie={this.state.movie} /> : null}
       </div>
     );
   }
