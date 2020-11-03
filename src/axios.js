54426b4dc58d91527e273c1dd548400bae3689d0
Olaf <olafschw@3c22fb7e214b.ant.amazon.com>
2020-11-03 11:57:40 -0800

    Test axios config

--- src/axios.js
+++ src/axios.js
@@ -3,6 +3,6 @@ import Axios from "axios";
 export const axios = Axios.create({
     baseURL: "https://cors-anywhere.herokuapp.com/"+"http://cinerama-env-2.eba-h2nnwj3t.us-west-2.elasticbeanstalk.com/api/",
     headers: {
-        "Content-type": "application/json"
+        "Content-type": "text/plain"
     }
 });
